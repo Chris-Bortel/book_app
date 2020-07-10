ea179b5e3746699e114296a7c53a97787efb011f
Chris-Bortel <bortelcc19@gmail.com>
2020-07-10 15:32:30 -0700

    server is readable

--- server.js
+++ server.js
@@ -8,49 +8,74 @@ const express = require('express');
 // const pg = require('pg');
 const cors = require('cors');
 const morgan = require('morgan');
-
-// Dependencies
-// const client = new pg.Client(process.env.something);
 const app = express();
+// const client = new pg.Client(process.env.something);
+
 app.use(cors());
-app.use(morgan("dev"));
-
-// Connects server.js to ejs files
-app.set('view engine', 'ejs');
-
+app.use(morgan('dev'));
+// EJS Connects server.js to views
+app.set('view engine', 'ejs'); 
 // Allows us to get form POST
 app.use(express.urlencoded({ extended: true }));
-
 // static ensures everything stays the same
 app.use(express.static('./public'));
 
+//////////////////////////////////////////////////
+////// Routes
+//////////////////////////////////////////////////
+app.use('/', handleHomePage);
+app.use('/searches', handleSearches); // 304 error
 
-// Runs whenever we have an error
-app.use(errorHandler);
-app.get('/', routeHandler);
+app.use('*', handleNotFound); // any route not found
+app.use(handleError);
 
+
+//////////////////////////////////////////////////
+////// Start Server
+//////////////////////////////////////////////////
+
+app.listen(process.env.PORT, () => console.log(`Server is running on ${process.env.PORT}`));
+
+
+//////////////////////////////////////////////////
+////// Route Handlers
+//////////////////////////////////////////////////
 
 app.get('/hello', (req, res) => {
   res.render('index');
 });
 
-// any route not found
-app.use('*', notFoundHandler);
-
-
-function routeHandler(req, res) {
-  res.status(200).send('Route working')
+function handleHomePage(req, res) {
+  res.status(200).send(`('/') Route working`);
+  console.log('this is searches');
 }
 
-// If the route is not found, this will run
-function notFoundHandler(req, res) {
-  res.status(404).send('404 Error: This is not the route you are looking for')
+function handleSearches(req, res) {
+  res.status(200).send(`searches`);
 }
 
-function errorHandler(error, req, res, next) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function handleNotFound(req, res) {
+  res.status(404).send('404 Error: This is not the route you are looking for');
+}
+
+function handleError(error, req, res, next) {
   console.log(error);
   res.status(500).send('500 Fly you fools!');
 }
 
 
-app.listen(process.env.PORT, () => console.log(`Server is running on ${process.env.PORT}`));
+
