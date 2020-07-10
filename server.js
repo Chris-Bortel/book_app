d087f38479f3d04dab11a4a4c16d615f80d2234a
Chris-Bortel <bortelcc19@gmail.com>
2020-07-09 19:28:54 -0700

    basic server is running

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 'use strict';
 
+require('dotenv').config();
+
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+app.use(cors());
+app.use(morgan("dev"));
+
+// Allows us to get form POST
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT, () => console.log(`Server is running on ${process.env.PORT}`));
