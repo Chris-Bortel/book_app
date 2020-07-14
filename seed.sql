ab37f48c677826c30e7ccff96972243f9249b6c8
Chris-Bortel <bortelcc19@gmail.com>
2020-07-13 17:30:30 -0700

    fixed the table order

--- seed.sql
+++ seed.sql
@@ -1,6 +1,6 @@
 INSERT INTO books
   (author, title, isbn, image_url, description, bookshelf)
 VALUES
-  ('Random Author', 'Random title', 'Random isbn', 'https:
-//i.imgur.com/J5LVHEL.jpg', 'blah blah description', 'random bookshelf')
+  ('Author', 'title', 'isbn', 'https:
+//i.imgur.com/J5LVHEL.jpg', 'description', 'bookshelf: adventure')
 RETURNING *;
